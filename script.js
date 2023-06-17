9ffe3e423c5cb07c99c688f09b2f38217c6a77d6
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-06-17 22:59:55 +0800

    Add hovered style and eventListener for mouseleave

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ for (let i = 0; i < 16; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         container.appendChild(div);
+
+        div.addEventListener('mouseleave', function() {
+            div.classList.add('hovered');
+        });
     }
 }
