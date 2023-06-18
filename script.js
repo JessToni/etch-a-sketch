8e9baa60e4a5848d08094f04de65a8b2f9fb0ccd
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-06-18 15:34:39 +0800

    Add clear and resize grid functionality to button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Create a function that creates a grid using square divs
 const container = document.getElementById('container');
+const inputBtn = document.getElementById('canvas-btn');
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
@@ -13,8 +14,26 @@ for (let i = 0; i < 16; i++) {
     }
 }
 
-const inputBtn = document.getElementById('canvas-btn');
-
 inputBtn.addEventListener('click', function () {
-    const userInput = prompt('Please enter the number of squares on each side you want to have for the canvas:');
+    const gridSize = parseInt(prompt('Please enter the number of squares on each side (1-100):'));
+    /*if (typeof gridSize !== number) {
+
+    }*/
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+            container.appendChild(div);
+    
+            div.addEventListener('mouseleave', function() {
+                div.classList.add('hovered');
+            });
+        }
+    }
 });
