084377b651e948353dd559e72fa2fed8ef683e27
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-06-19 17:00:26 +0800

    Create a getRandomColor function

--- script.js
+++ script.js
@@ -48,3 +48,13 @@ inputBtn.addEventListener('click', function () {
         }
     }
 });
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+
+    return color;
+}
