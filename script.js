4cb631148b3c8c91390870a477b1e542ca4125d5
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-06-25 15:36:43 +0800

    Fix typo in colorSquares and add comments to code

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-//Create a function that creates a grid using square divs
+//Create constant variables that references HTML elements.
 const container = document.getElementById('container');
 const inputBtn = document.getElementById('canvas-btn');
 const clearBtn = document.getElementById('clear-btn');
 const magicBtn = document.getElementById('magic-btn');
 const shadeBtn = document.getElementById('shade-btn');
 
+//Sets up a global variable grid size and default color choice and grid on start up.
 let gridSize;
 colorChoice('black');
 gridMaker();
 
+//A function that creates a grid using square divs.
 function gridMaker(gridSize = 16, color = "#000000") {
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
@@ -23,24 +25,28 @@ function gridMaker(gridSize = 16, color = "#000000") {
     }
 }
 
+//A function that determines the color of square divs when mouse hovers over them.
 function colorSquares() {
     if (color === 'black') {
         this.style.backgroundColor = 'black';
     } else {
-        this.style.backgroundColor = `hsl(${Math.random * 360}), 100%, 50%`;
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     }
 }
 
+//A function that establishes a global variable color for colorSquares function.
 function colorChoice(colorChoice) {
     color = colorChoice;
 }
 
+//Function that clears grid.
 function clearGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
 
+//Function that takes user input and filters them
 function takeUserInput() {
     const userInput = prompt('Please enter the number of squares on each side (1-100):');
 
@@ -55,6 +61,8 @@ function takeUserInput() {
         return;
     }
 }
+
+//Adds functionality to buttons on click
 
 inputBtn.addEventListener('click', function () {
     takeUserInput();
